77731bb2c0f8615c815e8ceafc156fa201d7430a
Himendra <himendrafadil@gmail.com>
2026-01-16 22:27:42 +0700

    edit sedikitlagi

--- server/index.js
+++ server/index.js
@@ -3,10 +3,8 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-// Izinkan siapa saja (termasuk React) mengakses data ini
 app.use(cors());
 
-// DATA DUMMY (Ceritanya ini database kita sementara)
 const menuDimsum = [
     { id: 1, nama: "Siomay Ayam", harga: 15000, stok: 10 },
     { id: 2, nama: "Hakau Udang", harga: 20000, stok: 5 },
@@ -14,13 +12,20 @@ const menuDimsum = [
     { id: 4, nama: "Bakpao Telur Asin", harga: 17000, stok: 12 },
 ];
 
-// ROUTE: Saat React minta ke alamat '/api/menu', kasih data di atas
 app.get('/api/menu', (req, res) => {
-    console.log("Ada yang minta data dimsum!"); // Cek di terminal nanti
+    console.log("Ada yang minta data dimsum!");
     res.json(menuDimsum);
 });
 
-// Nyalakan Server
-app.listen(PORT, () => {
-    console.log(`Server Dimsum berjalan di http://localhost:${PORT}`);
-});
+// --- 👇 GANTI BAGIAN BAWAH INI YAA 👇 ---
+
+// 1. (BARU) Kita harus "ekspor" app ini supaya Vercel bisa menjalankannya
+module.exports = app;
+
+// 2. (BARU) Cek: Apakah kode ini sedang dijalankan langsung (di laptop)?
+// Kalau iya, baru kita nyalakan server manual.
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server Dimsum berjalan di http://localhost:${PORT}`);
+    });
+}
